db4ec762620506a49bb3178296996c876c282f1b
Алексей Храмов <ahramov@zuzex.com>
2024-02-05 16:59:52 +0300

    add babel, add i18n extract

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -7,6 +7,22 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     test: /\.svg$/,
     use: ["@svgr/webpack"],
   };
+  const babelLoader = {
+    test: /\.(js|ts|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-env"],
+        plugins: [
+          [
+            "i18next-extract",
+            { locales: ["ru", "en"], keyAsDefaultValue: true },
+          ],
+        ],
+      },
+    },
+  };
   const fileLoader = {
     test: /\.(png|jpe?g|gif)$/i,
     use: [
@@ -42,5 +58,5 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     exclude: /node_modules/,
   };
 
-  return [typescriptLoader, cssLoader, svgLoader, fileLoader];
+  return [babelLoader, typescriptLoader, cssLoader, svgLoader, fileLoader];
 }
